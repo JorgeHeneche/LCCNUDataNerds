6f9c21829b943b2b9063fc5670aa9d3bf75bff7c
Jorge Heneche <89480558+JorgeHeneche@users.noreply.github.com>
2023-10-18 18:32:02 -0500

    Update Ch2Ex.sql

--- Exercises/Course1/Ch2Ex.sql
+++ Exercises/Course1/Ch2Ex.sql
@@ -1,1 +1,8 @@
+--Use the Purchasing.Vendor table in AdventureWorks
+--Locate all vendors whose name starts with the letter "C"
+--Filter the reulsts further to just those that also include the word "Bike" or "Bicycle"
 
+SELECT Name
+FROM Purchasing.Vendor
+WHERE Name LIKE 'C%' AND Name LIKE '%Bike%'
+		OR Name LIKE 'C%' AND Name LIKE '%Bicycle%';
